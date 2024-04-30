6695b7977dff2a33b2c94fc7d5dbf5abbc3162ce
Hijasahammed <hijasahammedp@gmail.com>
2024-04-30 17:17:53 +0530

    welcome page implemented

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,18 @@ import Login from './components/login';
 import Signup from './components/signup';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './components/home';
 
 function App() {
   const [users, setUsers] = useState([]);
 
   return (
     <BrowserRouter>
+   
       <Routes>
         <Route exact path="/" element={<Login users={users} />} />
         <Route path="/signup" element={<Signup setUsers={setUsers} />} />
+        <Route path="/welcome" element={ <Home/>}></Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ src/components/home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Welcome = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-gray-800 mb-4">Welcome to Our Sample Website</h1>
+      <p className="text-lg text-gray-600 text-center max-w-lg">
+        We're delighted to have you here. This is a site made by login and signup using with react JS. It made only to deliver to you!
+      </p>
+    </div>
+  );
+};
+
+export default Welcome;
